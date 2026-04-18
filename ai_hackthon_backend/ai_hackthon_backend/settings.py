e503ab1dbc8d34f744555838c46561fc329d63c0
Abdullah Ejaz <abdullahejaz123789@gmail.com>
2026-04-18 12:46:52 +0500

    Integrated Gemini AI classification into Django backend and React frontend

--- ai_hackthon_backend/ai_hackthon_backend/settings.py
+++ ai_hackthon_backend/ai_hackthon_backend/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+GEMINI_API_KEY = 'AIzaSyAi-tZ2x0m4oecWJZtBV8arhTsT7KWW9Ic'
--- ai_hackthon_backend/api/urls.py
+++ ai_hackthon_backend/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('classify_email/', views.classify_email, name='classify_email'),
 ]
